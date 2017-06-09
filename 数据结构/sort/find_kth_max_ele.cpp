85da0f37b872d02cb860e77d1156ffaca5c80621
陈勇 <chenyong@bmp.hust.edu.cn>
2017-06-09 11:47:30 +0800

    quick sort problem

--- 数据结构/sort/find_kth_max_ele.cpp
+++ 数据结构/sort/find_kth_max_ele.cpp
@@ -23,12 +23,40 @@ int partition(int a[], int p, int r)
 			swap(a[i], a[j]);
 		}
 	}
-	swap(a[j], a[p]);
+	if(a[j] <= a[p])
+		swap(a[j], a[p]);
 	return j;
 }
 
 int find_k_max_ele(int a[], int len, int k)
 {
+
+	/*
+	void quick_sort(int arr[], int left, int right)
+	{
+	if (left >= right)
+	{
+	return;
+	}
+	int i = left;
+	int j = right + 1;
+	do
+	{
+	do i++; while (arr[i] < arr[left]);
+	do j--; while (arr[j] > arr[left]);
+	if ( i < j)
+	{
+	swap(arr[i], arr[j]);
+	}
+	} while ( i < j);
+	swap(arr[j], arr[left]);
+	quick_sort( arr, left, j-1 );
+	quick_sort(arr, j +1, right);
+	}
+	
+	*/
+
+
 	int real_index = len - k;
 	int part_index = partition(a, 0, len-1);
 	while (part_index != real_index)
@@ -47,8 +75,8 @@ int find_k_max_ele(int a[], int len, int k)
 
 int main()
 {
-	int a[] = {5,2,4,1,3};
-	int result = find_k_max_ele(a, ARR_LEN(a), 2);
+	int a[] = {5,2,4,1,3,10000};
+	int result = find_k_max_ele(a, ARR_LEN(a)-1, 2);
 	cout << result << endl;
 	getchar();
 }
